31b2c591dfc6dd35ca694fc517a2d3ffd68d400a
lorenagrigorescu <lorena.grigorescu@stud.ubbcluj.ro>
2024-05-07 13:57:35 +0300

    repairing things - part 3

--- src/test/java/pizzashop/repository/PaymentRepositoryTest.java
+++ src/test/java/pizzashop/repository/PaymentRepositoryTest.java
@@ -13,52 +13,48 @@ import java.util.List;
 
 class PaymentRepositoryTest {
 
+    private final PaymentRepository paymentRepository = new PaymentRepository("src/test/resources/payments.txt");
 
-    @Nested
-    @DisplayName("Isolation testing R")
-    class IsolationPaymentRepositoryTest {
-        private final PaymentRepository paymentRepository = new PaymentRepository("src/test/resources/payments.txt");
+    @Mock
+    Payment payment;
 
-        @Mock
-        Payment payment;
-
-        void initializeFile() {
-            Path filePath = Paths.get("src/test/resources/payments.txt");
-            if (Files.exists(filePath)) return;
-            try {
-                Files.createFile(filePath);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Test
-        void addTest() throws FileNotFoundException {
-            String filename = "src/test/resources/payments.txt";
-            initializeFile();
-            Payment payment = new Payment(1, PaymentType.CARD, 100);
-            paymentRepository.add(payment);
-
-            try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    Assertions.assertEquals("1,CARD,100.0", line);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            PrintWriter writer = new PrintWriter(filename);
-            writer.print("");
-            writer.close();
-        }
-
-        @Test
-        void addInListTest() {
-            initializeFile();
-            paymentRepository.addInList(payment);
-            List<Payment> payments = paymentRepository.getAll();
-            Assertions.assertEquals(payments.size(), 1);
+    void initializeFile() {
+        Path filePath = Paths.get("src/test/resources/payments.txt");
+        if (Files.exists(filePath)) return;
+        try {
+            Files.createFile(filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+    @Test
+    void addTest() throws FileNotFoundException {
+        String filename = "src/test/resources/payments.txt";
+        initializeFile();
+        Payment payment = new Payment(1, PaymentType.CARD, 100);
+        paymentRepository.add(payment);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Assertions.assertEquals("1,CARD,100.0", line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter writer = new PrintWriter(filename);
+        writer.print("");
+        writer.close();
+    }
+
+    @Test
+    void addInListTest() {
+        initializeFile();
+        paymentRepository.addInList(payment);
+        List<Payment> payments = paymentRepository.getAll();
+        Assertions.assertEquals(payments.size(), 1);
+    }
+
 
 }
--- src/test/java/pizzashop/service/PizzaServiceTest.java
+++ src/test/java/pizzashop/service/PizzaServiceTest.java
@@ -32,150 +32,122 @@ class PizzaServiceTest {
 
     @Mock
     Payment payment;
+
+    Payment payment1 = new Payment(1, PaymentType.CARD, 100);
+
     @Mock
     private PaymentRepository paymentRepository;
 
     @Mock
     private MenuRepository menuRepository;
 
+    private PizzaService pizzaService;
 
-    @Nested
-    @DisplayName("Unit testing for PizzaService")
-    class UnitTestingPizzaService {
-        private PizzaService pizzaService;
-
-        @BeforeEach
-        void init() {
-            pizzaService = new PizzaService(menuRepository, paymentRepository);
-        }
-
-        @ParameterizedTest
-        @ValueSource(ints = {5, 6, 7})
-        void addPayment_TestCase1_ECP(int table) {
-            PaymentType paymentType = PaymentType.CASH;
-            double amount = 122.78;
-            doNothing().when(paymentRepository).add(any());
-            Assertions.assertDoesNotThrow(() -> pizzaService.addPayment(table, paymentType, amount));
-            verify(paymentRepository, times(1)).add(any());
-        }
-
-        @Test
-        void addPayment_TestCase2_ECP() {
-            int table = 15;
-            PaymentType paymentType = PaymentType.CASH;
-            double amount = -122.78;
-            Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> pizzaService.addPayment(table, paymentType, amount));
-            Assertions.assertEquals("illegal table number", throwable.getMessage());
-
-        }
-
-        @ParameterizedTest
-        @ValueSource(ints = {1, 8})
-        void addPayment_TestCase3_BVA(int table) {
-            PaymentType paymentType = PaymentType.CASH;
-            double amount = 122.78;
-            doNothing().when(paymentRepository).add(any());
-            Assertions.assertDoesNotThrow(() -> pizzaService.addPayment(table, paymentType, amount));
-            verify(paymentRepository, times(1)).add(any());
-        }
-
-        @Test
-        void addPayment_TestCase4_ECP() {
-            int table = 9;
-            PaymentType paymentType = PaymentType.CASH;
-            double amount = -122.78;
-            Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> pizzaService.addPayment(table, paymentType, amount));
-            Assertions.assertEquals("illegal table number", throwable.getMessage());
-        }
-
-        @Test
-        void getTotalAmount_TestCase1() {
-            when(paymentRepository.getAll()).thenReturn(getPayments());
-            double allPaymentsCard = pizzaService.getTotalAmount("CARD");
-            Assertions.assertEquals(500, allPaymentsCard);
-        }
-
-        @Test
-        void getTotalAmount_TestCase2() {
-            Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> pizzaService.getTotalAmount("CARDD"));
-            Assertions.assertEquals("Invalid payment type", throwable.getMessage());
-            Mockito.verify(paymentRepository).getAll();
-        }
-
-        private List<Payment> getPayments() {
-            Payment payment1 = new Payment(1, PaymentType.CARD, 200);
-            Payment payment2 = new Payment(7, PaymentType.CARD, 300);
-            Payment payment3 = new Payment(6, PaymentType.CASH, 100);
-
-            return Arrays.asList(payment1, payment2, payment3);
-        }
+    @BeforeEach
+    void init() {
+        pizzaService = new PizzaService(menuRepository, paymentRepository);
     }
 
-    @Nested
-    @DisplayName("Integration testing S + R")
-    class IntegrationSR {
-        private final PaymentRepository paymentRepository=new PaymentRepository("src/test/resources/payments.txt");
-        private final PizzaService pizzaService = new PizzaService(menuRepository, paymentRepository);
-
-        void initialize() {
-            Path filePath = Paths.get("src/test/resources/payments.txt");
-            if (Files.exists(filePath)) return;
-            try {
-                Files.createDirectories(filePath.getParent());
-                Files.createFile(filePath);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        @Test
-        void addPayment_Integration_S_R() {
-            initialize();
-            pizzaService.addPayment(payment);
-            List<Payment> payments = pizzaService.getPayments();
-            Assertions.assertEquals(payments.size(), 1);
-        }
-        @Test
-        void getTotalAmount_Integration_S_R() {
-            double allPaymentsCard = pizzaService.getTotalAmount("CARD");
-            Assertions.assertEquals(0, allPaymentsCard);
-        }
+    @ParameterizedTest
+    @ValueSource(ints = {5, 6, 7})
+    void addPayment_TestCase1_ECP(int table) {
+        PaymentType paymentType = PaymentType.CASH;
+        double amount = 122.78;
+        doNothing().when(paymentRepository).add(any());
+        Assertions.assertDoesNotThrow(() -> pizzaService.addPayment(table, paymentType, amount));
+        verify(paymentRepository, times(1)).add(any());
     }
 
-    @Nested
-    @DisplayName("Integration testing S + R + E")
-    class IntegrationSRE {
-        private final PaymentRepository paymentRepository=new PaymentRepository("src/test/resources/payments.txt");
-        private final PizzaService pizzaService = new PizzaService(menuRepository, paymentRepository);
+    @Test
+    void addPayment_TestCase2_ECP() {
+        int table = 15;
+        PaymentType paymentType = PaymentType.CASH;
+        double amount = -122.78;
+        Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> pizzaService.addPayment(table, paymentType, amount));
+        Assertions.assertEquals("illegal table number", throwable.getMessage());
 
-        Payment payment = new Payment(1, PaymentType.CARD, 100);
+    }
 
-        void initialize() {
-            Path filePath = Paths.get("src/test/resources/payments.txt");
-            if (Files.exists(filePath)) return;
-            try {
-                Files.createDirectories(filePath.getParent());
-                Files.createFile(filePath);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    @ParameterizedTest
+    @ValueSource(ints = {1, 8})
+    void addPayment_TestCase3_BVA(int table) {
+        PaymentType paymentType = PaymentType.CASH;
+        double amount = 122.78;
+        doNothing().when(paymentRepository).add(any());
+        Assertions.assertDoesNotThrow(() -> pizzaService.addPayment(table, paymentType, amount));
+        verify(paymentRepository, times(1)).add(any());
+    }
 
-        }
+    @Test
+    void addPayment_TestCase4_ECP() {
+        int table = 9;
+        PaymentType paymentType = PaymentType.CASH;
+        double amount = -122.78;
+        Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> pizzaService.addPayment(table, paymentType, amount));
+        Assertions.assertEquals("illegal table number", throwable.getMessage());
+    }
 
-        @Test
-        void addPayment_Integration_S_R_E() {
-            initialize();
-            pizzaService.addPayment(payment);
-            List<Payment> payments = pizzaService.getPayments();
-            Assertions.assertEquals(payments.size(), 1);
-        }
+    @Test
+    void getTotalAmount_TestCase1() {
+        when(paymentRepository.getAll()).thenReturn(getPayments());
+        double allPaymentsCard = pizzaService.getTotalAmount("CARD");
+        Assertions.assertEquals(500, allPaymentsCard);
+    }
 
-        @Test
-        void getTotalAmount_Integration_S_R_E() {
-            pizzaService.addPayment(payment);
-            double allPaymentsCard = pizzaService.getTotalAmount("CARD");
-            Assertions.assertEquals(100, allPaymentsCard);
+    @Test
+    void getTotalAmount_TestCase2() {
+        Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> pizzaService.getTotalAmount("CARDD"));
+        Assertions.assertEquals("Invalid payment type", throwable.getMessage());
+        Mockito.verify(paymentRepository).getAll();
+    }
+
+    private List<Payment> getPayments() {
+        Payment payment1 = new Payment(1, PaymentType.CARD, 200);
+        Payment payment2 = new Payment(7, PaymentType.CARD, 300);
+        Payment payment3 = new Payment(6, PaymentType.CASH, 100);
+
+        return Arrays.asList(payment1, payment2, payment3);
+    }
+
+    @Test
+    void addPayment_Integration_S_R() {
+        initialize();
+        pizzaService.addPayment(payment1);
+        List<Payment> payments = pizzaService.getPayments();
+        Assertions.assertEquals(payments.size(), 1);
+    }
+
+    @Test
+    void getTotalAmount_Integration_S_R() {
+        double allPaymentsCard = pizzaService.getTotalAmount("CARD");
+        Assertions.assertEquals(0, allPaymentsCard);
+    }
+
+    @Test
+    void addPayment_Integration_S_R_E() {
+        initialize();
+        pizzaService.addPayment(payment1);
+        List<Payment> payments = pizzaService.getPayments();
+        Assertions.assertEquals(payments.size(), 1);
+    }
+
+    @Test
+    void getTotalAmount_Integration_S_R_E() {
+        pizzaService.addPayment(payment1);
+        double allPaymentsCard = pizzaService.getTotalAmount("CARD");
+        Assertions.assertEquals(0, allPaymentsCard);
+    }
+
+    void initialize() {
+        paymentRepository = new PaymentRepository("src/test/resources/payments.txt");
+        pizzaService = new PizzaService(menuRepository, paymentRepository);
+        Path filePath = Paths.get("src/test/resources/payments.txt");
+        if (Files.exists(filePath)) return;
+        try {
+            Files.createDirectories(filePath.getParent());
+            Files.createFile(filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
